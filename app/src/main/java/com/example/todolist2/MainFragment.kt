3cb9bd58473fb5decc31c3b0d54be4accb708071
KirillAle <kombat90@bk.ru>
2024-09-13 22:44:39 +0400

    Добавил нажатие на элемент списка

--- app/src/main/java/com/example/todolist2/MainFragment.kt
+++ app/src/main/java/com/example/todolist2/MainFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.core.os.bundleOf
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -43,19 +44,7 @@ class MainFragment : Fragment(), OnItemClickListener {
     private fun addItems() {
         val items = listOf(
             ItemModel("Task 1", "Description for task 1"),
-            ItemModel("Task 2", "Description for task 2"),
-            ItemModel("Task 3", "Description for task 3"),
-            ItemModel("Task 4", "Description for task 4"),
-            ItemModel("Task 5", "Description for task 5"),
-            ItemModel("Task 6", "Description for task 6"),
-            ItemModel("Task 7", "Description for task 7"),
-            ItemModel("Task 8", "Description for task 8"),
-            ItemModel("Task 9", "Description for task 9"),
-            ItemModel("Task 10", "Description for task 10"),
-            ItemModel("Task 11", "Description for task 11"),
-            ItemModel("Task 12", "Description for task 12"),
-            ItemModel("Task 13", "Description for task 13"),
-            ItemModel("Task 14", "Description for task 14"),
+
         )
         items
             .filter { it.itemName.isNotEmpty() && it.itemDescription.isNotEmpty() }
