761bcc6c72dd5553516165b0e3408f5b19a1bb0e
KirillAle <kombat90@bk.ru>
2024-11-12 13:36:53 +0400

    #14 решил задач task1-2 по data class

--- kotlin_practice/src/main/java/com/example/kotlin_practice/data/task_1.kt
+++ kotlin_practice/src/main/java/com/example/kotlin_practice/data/task_1.kt
@@ -4,6 +4,7 @@ data class Person(val name: String, val age: Int)
 
 fun updateAge(age: Int): Person {
     val person = Person("Alice", 30)
+    person.copy(age = 22)
     // TODO: Создайте копию person с новым возрастом
     return person.copy(age = age)
 }
--- kotlin_practice/src/main/java/com/example/kotlin_practice/data/task_2.kt
+++ kotlin_practice/src/main/java/com/example/kotlin_practice/data/task_2.kt
@@ -1,11 +1,23 @@
 package com.example.kotlin_practice.data
 
-data class Point(val x: Int, val y: Int)
-
-// TODO: Переопределите метод equals для сравнения только координат x
+data class Point(val x: Int, val y: Int) {
+    override fun equals(other: Any?): Boolean {
+        if (other is Point)
+            return this.x == other.x
+        return false
+//        не совсем понятно, как и в какой момент мы принимаем для себя, что
+//        this это p1, в other это p2. если это просто по ходу программы так,
+//        как двигается компилятор по строкам кода сверзу вниз по полям, то ок,
+//        в противном случае не ясно. может ли this быть p2? или other - это любое
+//        другое значение, с которым идет сравнение в рамках выражения println(p1 == p2)?
+    }
+}
 
 fun main() {
     val p1 = Point(1, 2)
     val p2 = Point(1, 3)
+    val p3 = Point(2, 4)
+
     println(p1 == p2) // Должно вывести: true
+    println(p2 == p3)
 }
