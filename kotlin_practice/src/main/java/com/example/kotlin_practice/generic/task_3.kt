23a7d8f5fdd33095095813499bc532d4350d688d
KirillAle <kombat90@bk.ru>
2024-11-16 14:13:10 +0400

    #17 решил задач task 3 по generic но понял мало. запутался в типах Any и T приведении их и inline fun <reified T> совсем не понял. понял, что есть ограничения на  переиспользование таких фекнций

--- kotlin_practice/src/main/java/com/example/kotlin_practice/generic/task_3.kt
+++ kotlin_practice/src/main/java/com/example/kotlin_practice/generic/task_3.kt
@@ -1,19 +1,26 @@
 package com.example.kotlin_practice.generic
 
 class Repository {
-    private val data = mutableMapOf<String, Any>()
+    val data = mutableMapOf<String, Any>()
 
     // TODO: Реализуйте обобщенный метод save
+    fun <T> save(key: String, value: T) {
+        data[key] = value as Any
+    }
 
     // TODO: Реализуйте обобщенный метод get
+    inline fun <reified T> get(key: String): T? {
+        return data[key] as? T
+
+    }
 }
 
 fun main() {
-//    val repo = Repository()
-//    repo.save("user1", User("Alice"))
-//    val user: User? = repo.get("user1")
-//
-//    println(user) // Ожидаемый вывод: User(name=Alice)
+    val repo = Repository()
+    repo.save("user1", User("Alice"))
+    val user: User? = repo.get("user1")
+
+    println(user) // Ожидаемый вывод: User(name=Alice)
 }
 
 data class User(val name: String)
