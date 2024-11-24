d142fabb0325aed11a15a531523add3a1085e611
KirillAle <kombat90@bk.ru>
2024-11-24 22:37:49 +0400

    #21 продвижение сильно замедлилось. я только немного продаинулся в задаче 1 изи. прододжаю штурм

--- kotlin_practice/src/main/java/com/example/kotlin_practice/exam/easy/task_1.kt
+++ kotlin_practice/src/main/java/com/example/kotlin_practice/exam/easy/task_1.kt
@@ -2,6 +2,7 @@ package com.example.kotlin_practice.exam.easy
 
 import java.math.BigDecimal
 import java.time.Instant
+import kotlin.system.measureTimeMillis
 
 data class Transaction (
     val id: String,
@@ -19,21 +20,36 @@ data class Transaction (
 class TransactionProcessor {
     fun process(transactions: List<Transaction>) {
         // Ваш код здесь
-        val groupedTransactions = transactions.groupBy { it.type }
-        val sumOfAmount = groupedTransactions.mapValues {
-                (_,amountList)-> amountList.sumOf { it.amount }
+        val listTime = measureTimeMillis {
+            val groupedTransactions = transactions
+                .groupBy { it.type }
+            val sumOfAmount = groupedTransactions
+                .mapValues { (_, amountList) ->
+                amountList.sumOf { it.amount }
+            }
+            println("Сгруппированные и просуммированные транзакци: $sumOfAmount")
         }
-        println(groupedTransactions)
+        println("Время обработки List: $listTime")
+
+        //Использование сиквенс
+        val sequenceTime = measureTimeMillis {
+            val sequenceTransaction = transactions
+                .asSequence()
+                .groupBy { it.type }
+                .mapValues { (_, amountList) -> amountList.sumOf { it.amount } }
+            println("Использование Sequence: $sequenceTransaction")
+        }
+        println("Время обработки Sequence:$sequenceTime")
     }
 }
-fun main(){
-    val ex = listOf(
-        Transaction("1", BigDecimal("12.1"), "trans", timestamp = Instant.now()),
-        Transaction("2", BigDecimal("12424.1"), "change", timestamp = Instant.now()),
-        Transaction("3", BigDecimal("56731.1"), "trans", timestamp = Instant.now()),
-        Transaction("4", BigDecimal("12.1"), "change", timestamp = Instant.now())
-    )
-    val statrOperation = TransactionProcessor().process(ex)
-    println(statrOperation)
 
-}
+    fun main() {
+        val ex = listOf(
+            Transaction("1", BigDecimal("12.1"), "trans", timestamp = Instant.now()),
+            Transaction("2", BigDecimal("12424.1"), "change", timestamp = Instant.now()),
+            Transaction("3", BigDecimal("56731.1"), "trans", timestamp = Instant.now()),
+            Transaction("4", BigDecimal("12.1"), "change", timestamp = Instant.now())
+        )
+        val statrOperation = TransactionProcessor().process(ex)
+    }
+
