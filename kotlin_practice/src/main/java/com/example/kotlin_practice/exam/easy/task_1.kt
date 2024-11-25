deb00453dcf66d8f92d9233b879db9c1d67319e3
KirillAle <kombat90@bk.ru>
2024-11-25 08:48:20 +0400

    #22 завершил задачу 1 изи, перехожу к следующей

--- kotlin_practice/src/main/java/com/example/kotlin_practice/exam/easy/task_1.kt
+++ kotlin_practice/src/main/java/com/example/kotlin_practice/exam/easy/task_1.kt
@@ -43,6 +43,30 @@ class TransactionProcessor {
     }
 }
 
+class TransactionIterator(
+    val transactions: List<Transaction>,
+    val filterType: String):
+    Iterator<Transaction> {
+        var currentIndex = 0
+
+    override fun hasNext(): Boolean {
+        while (currentIndex < transactions.size) {
+            if (transactions[currentIndex].type == filterType) {
+                return true
+            }
+            currentIndex++
+
+        }
+        return false
+    }
+
+    override fun next(): Transaction {
+        if (!hasNext()) throw NoSuchElementException("Болше нет элементов типа $filterType")
+        return transactions[currentIndex++]
+
+    }
+    }
+
     fun main() {
         val ex = listOf(
             Transaction("1", BigDecimal("12.1"), "trans", timestamp = Instant.now()),
@@ -50,6 +74,11 @@ class TransactionProcessor {
             Transaction("3", BigDecimal("56731.1"), "trans", timestamp = Instant.now()),
             Transaction("4", BigDecimal("12.1"), "change", timestamp = Instant.now())
         )
-        val statrOperation = TransactionProcessor().process(ex)
+        val startOperation = TransactionProcessor().process(ex)
+
+        val iterator = TransactionIterator(ex,"trans")
+        while (iterator.hasNext()){
+            println(iterator.next())
+        }
     }
 
