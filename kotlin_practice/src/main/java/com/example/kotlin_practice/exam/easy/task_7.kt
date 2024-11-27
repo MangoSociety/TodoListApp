d1acdc0aadc623401dfc95c3e427c4b582a2e8c8
KirillAle <kombat90@bk.ru>
2024-11-27 16:47:28 +0400

    #25 завершил задачу 7 изи, перехожу к следующей

--- kotlin_practice/src/main/java/com/example/kotlin_practice/exam/easy/task_7.kt
+++ kotlin_practice/src/main/java/com/example/kotlin_practice/exam/easy/task_7.kt
@@ -1,7 +1,22 @@
 package com.example.kotlin_practice.exam.easy
 
 // Задание: Использовать lazy для отложенной инициализации
-class User {
+class User(
+    val firstName: String,
+    val lastName: String
+) {
     // Изменить свойство fullInfo, которое будет вычисляться только при первом обращении
-    val fullInfo: String = ""
+    val fullInfo: String by lazy {
+        println("Pricessing...")
+        "$firstName $lastName"
+
+    }
+}
+
+fun main(){
+    val getFullInfo = User(firstName = "Lilu", lastName =  "Nafikova")
+
+
+    println("Full info: ${getFullInfo.fullInfo}")
+
 }
