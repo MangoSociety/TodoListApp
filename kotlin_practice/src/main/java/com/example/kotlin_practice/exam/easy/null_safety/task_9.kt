5f8c769af0bbd84f58ff3d845eac2d10fca9ef4e
KirillAle <kombat90@bk.ru>
2024-12-01 16:11:52 +0400

    #28 залип на задаче 9 на mapNotNull. я улових, что такой способ максимальнь гибкий, так как позволяет помимо фильтрации, еще и обрабатывать каждый отдельный элемент. Но я опять поплыл в типах. мзначально item у нас типа Т?, потом R? но по итогу мы возыращаем просто R. где сидит логическая ошибка? поругай меня

--- kotlin_practice/src/main/java/com/example/kotlin_practice/exam/easy/null_safety/task_9.kt
+++ kotlin_practice/src/main/java/com/example/kotlin_practice/exam/easy/null_safety/task_9.kt
@@ -6,9 +6,55 @@ class SafeList<T> {
 
     // Реализовать методы, которые безопасно обрабатывают null элементы:
     // 1. addIfNotNull: добавляет элемент только если он не null
+    fun addIfNotNull(newItem: T?): SafeList<T> {
+        if (newItem !== null) {
+            items.add(newItem)
+        }
+        return this
+    }
+
     // 2. filterNotNull: возвращает новый SafeList без null элементов
+    fun filterNotNull(): SafeList<T> {
+        val notNullItem = items.filterNotNull()
+        val notNullSafeList = SafeList<T>()
+        notNullItem.forEach { notNullSafeList.addIfNotNull(it) }
+        return notNullSafeList
+    }
+
     // 3. mapNotNull: преобразует элементы, пропуская null
+    fun <R : Any> mapNotNull(transform: (T?) -> R?): SafeList<R> {
+        val mappedItem = items.mapNotNull(transform)
+        val resultSafeList = SafeList<R>()
+        mappedItem.forEach { resultSafeList.addIfNotNull(it) }
+        return resultSafeList
+
+
+    }
+
+    override fun toString(): String {
+        return items.toString()
+    }
+
+
     // 4. firstOrNull: возвращает первый не-null элемент или null
 
     // Методы должны поддерживать chain-операции
+}
+
+fun main() {
+    val safeList = SafeList<String>()
+
+    safeList
+        .addIfNotNull("robert")
+        .addIfNotNull("foruh")
+        .addIfNotNull("pipin")
+        .addIfNotNull(null)
+        .addIfNotNull("goldy")
+
+    println(safeList.toString())
+
+    val filtredList = safeList.filterNotNull()
+    println(filtredList)
+
+    val filterdListByMap = safeList.mapNotNull { it?.length }
 }
